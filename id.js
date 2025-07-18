33ebb1fcc3e0dbf2e363e6083bb51192026d6759
TIGER-WAR2 <chowdhuryifran74@gmail.com>
2025-07-18 15:03:00 +0600

    Create id.js

---
+++ id.js
@@ -0,0 +1,16 @@
+// আপনার ফেসবুক কুকি এখানে পেস্ট করুন
+module.exports = {
+  appState: [
+    // উদাহরণ:
+    {
+      "key": "datr",
+      "value": "XXXXXXXXXXXXXX",
+      "domain": ".facebook.com",
+      "path": "/",
+      "hostOnly": false,
+      "creation": "2023-01-01T00:00:00.000Z",
+      "lastAccessed": "2023-01-01T00:00:00.000Z"
+    },
+    // আরও কুকি এখানে যোগ করুন
+  ]
+};
