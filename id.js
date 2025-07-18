bac924613c468418f5594742271ce57d3d093463
TIGER-WAR2 <chowdhuryifran74@gmail.com>
2025-07-18 15:45:15 +0600

    Update id.js

--- id.js
+++ id.js
@@ -1,16 +1,92 @@
-// আপনার ফেসবুক কুকি এখানে পেস্ট করুন
-module.exports = {
-  appState: [
-    // উদাহরণ:
+[
     {
-      "key": "datr",
-      "value": "XXXXXXXXXXXXXX",
-      "domain": ".facebook.com",
-      "path": "/",
-      "hostOnly": false,
-      "creation": "2023-01-01T00:00:00.000Z",
-      "lastAccessed": "2023-01-01T00:00:00.000Z"
+        "key": "dbln",
+        "value": "%7B%2261575175935681%22%3A%22Spg6rTKv%22%7D",
+        "domain": "facebook.com",
+        "path": "/login/device-based/",
+        "hostOnly": false,
+        "creation": "2025-07-18T09:44:41.031Z",
+        "lastAccessed": "2025-07-18T09:44:41.049Z"
     },
-    // আরও কুকি এখানে যোগ করুন
-  ]
-};
+    {
+        "key": "sb",
+        "value": "Es74Z-7KJfN90OX7GoNu2-zO",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-18T09:44:41.049Z",
+        "lastAccessed": "2025-07-18T09:44:41.049Z"
+    },
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-18T09:44:41.049Z",
+        "lastAccessed": "2025-07-18T09:44:41.049Z"
+    },
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-18T09:44:41.049Z",
+        "lastAccessed": "2025-07-18T09:44:41.049Z"
+    },
+    {
+        "key": "datr",
+        "value": "l4ZvaHvVyYvoiKJqby_LFTc0",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-18T09:44:41.049Z",
+        "lastAccessed": "2025-07-18T09:44:41.049Z"
+    },
+    {
+        "key": "wd",
+        "value": "1400x961",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-18T09:44:41.049Z",
+        "lastAccessed": "2025-07-18T09:44:41.049Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61575175935681",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-18T09:44:41.049Z",
+        "lastAccessed": "2025-07-18T09:44:41.049Z"
+    },
+    {
+        "key": "xs",
+        "value": "29%3AG7M4LJfnSA73gg%3A2%3A1752831826%3A-1%3A-1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-18T09:44:41.049Z",
+        "lastAccessed": "2025-07-18T09:44:41.049Z"
+    },
+    {
+        "key": "fr",
+        "value": "1okP3rkm7cdwVXYdZ.AWdcmRaj2e1Qcno1IbNRC9Fr3exEQG_dn5j_mIasiK_Q1IleMoo.BoehcZ..AAA.0.0.BoehdU.AWdi4bE1ozMdDQO1mxEeCQkMwLU",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-18T09:44:41.049Z",
+        "lastAccessed": "2025-07-18T09:44:41.049Z"
+    },
+    {
+        "key": "presence",
+        "value": "C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1752831830721%2C%22v%22%3A1%7D",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-18T09:44:41.049Z",
+        "lastAccessed": "2025-07-18T09:44:41.051Z"
+    }
+]
